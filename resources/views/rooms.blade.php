f2be5737b8b28a4813c742e74f2914d8e8f9e0a8
murkycricket <aldrencemarloparis@gmail.com>
2024-10-23 20:28:35 +0800

    test update

--- resources/views/rooms.blade.php
+++ resources/views/rooms.blade.php
@@ -1,5 +1,29 @@
 <x-layout>
-    <h1>Our Rooms</h1>
-
+    <div class="min-vh-100 bg-secondary d-flex">
+        <div class="container bg-primary">
+            <div class="col-md-6 bg-danger">
+                <h1 class="fs-1 fw-light fst italic">Our Rooms</h1>
+                <p class="fs-6 fw-light fst normal">
+                    At Hotel Tranquila, the rooms are designed to offer a serene escape from the bustle of daily life.
+                    Each room is elegantly furnished with warm, earthy tones and soft lighting, creating a peaceful and
+                    inviting ambiance. Plush bedding with crisp linens ensures a restful night’s sleep, while large
+                    windows offer picturesque views of lush gardens or the tranquil sea. Modern amenities like
+                    high-speed Wi-Fi, a flat-screen TV, and a luxurious bathroom with rainfall showers elevate the
+                    comfort. Some rooms also feature private balconies or terraces, perfect for enjoying a quiet morning
+                    coffee or evening breeze. Whether you're here to relax or work, the calm, minimalist design of Hotel
+                    Tranquila’s rooms promises a rejuvenating stay.
+                    <hr style="height: 10px;">
+                </p>
+                <div class="col-md-5 bg-secondary">
+                    <div class="container bg-warning">
+                        <img src="{{ asset('images/Room1.jpg') }}"style="width: 100%; height: 100%;" class="">
+                        <p class=" text-center fs-6 fw-light fst normal">
+                            Room 1
+                        <p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </x-layout>
